53f96677b41d8a834b7399f80efb52edbd81661e
toshiki goto <gotoshi.euphonium@gmail.com>
2024-03-20 11:39:45 +0900

    add led blinking sample

---
+++ synthesis_sample/led.sv
@@ -0,0 +1,17 @@
+module led (
+    input clk,
+    output led[4]
+);
+
+logic [26:0] counter;
+
+assign led[0] = counter[26];
+assign led[1] = counter[25];
+assign led[2] = counter[24];
+assign led[3] = counter[23];
+
+always @(posedge clk) begin
+    counter <= counter + 1;
+end
+
+endmodule
