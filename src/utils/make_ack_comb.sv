9cc02b5431d1f4c3e24c4d035705cab43ad6ea2f
toshiki goto <gotoshi.euphonium@gmail.com>
2024-05-31 13:28:14 +0900

    add is_ack field

--- src/utils/make_ack_comb.sv
+++ src/utils/make_ack_comb.sv
@@ -9,6 +9,7 @@ types::flit_t tmp_flit_out;
 // 現在ackの確認はflit_id, src_idのみで行うためそれ以外はコピー
 always_comb begin
     tmp_flit_out = flit_in;
+    tmp_flit_out.header.is_ack = 1;
     tmp_flit_out.header.src_id = flit_in.header.dst_id;
     tmp_flit_out.header.dst_id = flit_in.header.src_id;
 end
