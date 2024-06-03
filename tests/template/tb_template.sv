722792cb24ef305e301274e1727241ab2f31b707
toshiki goto <gotoshi.euphonium@gmail.com>
2024-06-03 23:08:04 +0900

    fix typo

--- tests/template/tb_template.sv
+++ tests/template/tb_template.sv
@@ -15,13 +15,13 @@ module tb_template ();
   end
 
   // input
-  logic input_tmplate;
+  logic input_template;
 
   // output
   logic output_template;
   logic output_not_template;
-  assign output_template = input_tmplate;
-  assign output_not_template = ~input_tmplate;
+  assign output_template = input_template;
+  assign output_not_template = ~input_template;
 
   // expected
   logic expected_template;
@@ -41,9 +41,8 @@ module tb_template ();
     @(posedge clk);
     rst_n = 1;
 
-    input_tmplate = 1;
+    input_template = 1;
     expected_template = 1;
-
     `LOCAL_TEST
 
     repeat (10) @(posedge clk);
