ae87539e91f5bcec4c50aeb706283d2fced77f3c
toshiki goto <gotoshi.euphonium@gmail.com>
2024-07-02 17:12:11 +0900

    rewrite generator

--- multiple_object_generater/src/main.rs
+++ multiple_object_generater/src/main.rs
@@ -1,7 +1,7 @@
+use clap::Parser;
 use multiple_object_generater::Args;
 use multiple_object_generater::NocConfig;
 use serde_json::from_str;
-use clap::Parser;
 
 fn read_json_file(args: &Args) -> NocConfig {
     let json_file = std::fs::read_to_string(&args.json_file).unwrap();
@@ -11,6 +11,12 @@ fn read_json_file(args: &Args) -> NocConfig {
 fn main() {
     let args = Args::parse();
     let mut noc_config = read_json_file(&args);
-    let system_verilog = noc_config.generate_system_verilog();
-    std::fs::write(&args.output_file, system_verilog).unwrap();
+    let verilog = if args.module_name.is_none() {
+        let module_name = args.output_file.split('.').next().unwrap();
+        let module_name = module_name.split('/').last().unwrap();
+        noc_config.generate_system_verilog(module_name)
+    } else {
+        noc_config.generate_system_verilog(&args.module_name.unwrap())
+    };
+    std::fs::write(&args.output_file, verilog).unwrap();
 }
